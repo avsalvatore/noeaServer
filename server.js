47e83a9da1ec67437fb72bf51009d484c492391b
avsalvatore <avsalvatore@gmail.com>
2015-04-27 19:58:54 -0400

    changed response.send(400) to response.send({})

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 	db = databaseConnection;
 });
 
-
+//finds restaurants within a certain lat/lng range of given range
 app.get('/findRestaurants', function(request, response) {
-	response.header("Access-Control-Allow-Origin", "*");
+	  response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "X-Requested-With");
-	response.set('Content-Type', 'text/html');
+	  response.set('Content-Type', 'text/html');
 
 	var mylat = request.query.mylat;
 	var mylng = request.query.mylng;
@@ -24,13 +24,12 @@ app.get('/findRestaurants', function(request, response) {
 
 	db.collection('restaurants', function (err, coll) {
 		if (err) {
-			response.send(404);
+			response.send({});
 		} else {
-			coll.find({lat: {$gt: mylat - range, $lt: mylat - range}, 
-				       lng: {$gt: mylng - range, mylng + range}}).toArray(
+			coll.find({lat: {$gt: mylat - range, $lt: mylat - range}, lng: {$gt: mylng - range, mylng + range}}).toArray(
   				function(error2, docs) {
   					if (error2) {
-  						response.send(400);
+  						response.send({});
   					} else if (docs.length > 0) {
   						response.send(docs);
   					} else {
@@ -41,6 +40,32 @@ app.get('/findRestaurants', function(request, response) {
 		}
 	});
 });
+
+//finds past and future 'NOEAs' based on login id
+app.get('/findnoas', function(request, response){
+	response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "X-Requested-With");
+  	var login = request.query.login;
+  
+  	if (login == null) {
+  		response.send({});
+  	} else {
+  		db.collection('noeas', function(error1, coll) {
+  			coll.find({'user_id': login}).toArray(
+  				function(error2, docs) {
+  					if (error2) {
+  						response.send({});
+  					} else if (docs.length > 0) {
+  						response.send(docs);
+  					} else {
+  					response.send({});
+  				}
+  			})
+  		});
+  	}
+});
+
+
 app.listen(app.get('port'), function() {
   console.log("NOEA app is running at localhost:" + app.get('port'));
 });
